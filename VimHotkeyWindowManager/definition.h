82d5f97b2355985c5df75dce38c7e66172e0f1c2
KikyoNanakusa <nanakusa.vrc@gmail.com>
2024-03-14 00:55:44 +0900

    refactor:Make definition.h

---
+++ VimHotkeyWindowManager/definition.h
@@ -0,0 +1,13 @@
+#pragma once
+#define WM_NOTIFYCATION (WM_USER + 100)
+#define HOTKEY_LEFT 1
+#define HOTKEY_DOWN 2
+#define HOTKEY_UP 3
+#define HOTKEY_RIGHT 4
+#define HOTKEY_MINIMIZE 5
+#define HOTKEY_RESTORE 6
+
+#define MENU_ACTIVATE 0x1
+#define MENU_EXIT 0x2
+
+#define NO_MINIMIZED_WINDOW 0x002
--- VimHotkeyWindowManager/main.cpp
+++ VimHotkeyWindowManager/main.cpp
@@ -1,17 +1,6 @@
 #include <Windows.h>
+#include "definition.h"
 
-#define WM_NOTIFYCATION (WM_USER + 100)
-#define HOTKEY_LEFT 1
-#define HOTKEY_DOWN 2
-#define HOTKEY_UP 3
-#define HOTKEY_RIGHT 4
-#define HOTKEY_MINIMIZE 5
-#define HOTKEY_RESTORE 6
-
-#define MENU_ACTIVATE 0x1
-#define MENU_EXIT 0x2
-
-#define NO_MINIMIZED_WINDOW 0x002
 
 HMENU hMenu;
 NOTIFYICONDATA nid;
