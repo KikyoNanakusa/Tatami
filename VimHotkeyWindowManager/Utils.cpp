db6790732a149499fd2d8f38cbd5fa31b45abd78
KikyoNanakusa <nanakusa.vrc@gmail.com>
2024-08-23 20:19:36 +0900

    feat: Add Utils file

---
+++ VimHotkeyWindowManager/Utils.cpp
@@ -0,0 +1,15 @@
+#include "Utils.h"
+
+std::string trim(const std::string& s) {
+    auto start = s.begin();
+    while (start != s.end() && std::isspace(*start)) {
+        start++;
+    }
+
+    auto end = s.end();
+    do {
+        end--;
+    } while (std::distance(start, end) > 0 && std::isspace(*end));
+
+    return std::string(start, end + 1);
+}
---
+++ VimHotkeyWindowManager/Utils.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <string>
+
+std::string trim(const std::string& s);
